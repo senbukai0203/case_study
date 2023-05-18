33881c804e06bff371df7150b87f5c829cf4b388
AB <senbukai0203@gmail.com>
2023-05-18 15:45:59 +0530

    tracker 4

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,17 @@
+import pytest
+class NotInRange(Exception) :
+    def __init__(self, message="value not in given range - by Oracle") :
+        self.message = message
+        super().__init__(self.message)
+
 def test_generic() :
     a = 30
     b = 40
 
     assert a == 30
+
+def test_generic_1() :
+    a=500;
+    with pytest.raises(NotInRange):
+        if a not in range(0,200):
+            raise NotInRange
